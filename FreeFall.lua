fc76ebf6dc5e3a6da32f44417aa7141fd706624d
StepBroFurious <87245695+StepBroFurious@users.noreply.github.com>
2021-11-15 21:03:43 -0800

    Add files via upload

---
+++ FreeFall.lua
@@ -0,0 +1,7 @@
+--Version 1.7
+loadstring(game:HttpGet("https://runed-profile.000webhostapp.com/FreeFall.lua"))()
+--[[
+Update Log:
+I decided to host a website and put the
+script on there so i can update the script instantly
+instead of waiting 5 minutes for github each time.
